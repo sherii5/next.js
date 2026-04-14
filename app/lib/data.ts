c30fb3cb774e0bff0b4bcc10a164d8916cf15287
Shahrzad Ali Khani Nejad <shnejad@windexnet.locale>
2026-04-14 14:01:08 +0200

    first commit

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,5 @@
 import postgres from 'postgres';
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 import {
   CustomerField,
   CustomersTableType,
@@ -9,19 +10,17 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+     console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
